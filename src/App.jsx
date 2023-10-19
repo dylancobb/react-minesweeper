02f3db9cb84d59d4ddea93e8a0140d1489719b3b
GeorgeKlemperer <george@klemperer.co.uk>
2023-10-19 12:47:01 +0100

    confetti added

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,60 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+import { Win } from './components/Win';
 
-import Board from "./components/Board"
-import Modal from "./components/Modal"
-import createBoard from "./utilities/boardGen"
+import Board from "./components/Board";
+import Modal from "./components/Modal";
+import createBoard from "./utilities/boardGen";
 
 const App = () => {
-  const [gameState, setGameState] = useState("notOver")
-  const rows = 8, cols = 8, mines = 10
-  const [remaining, setRemaining] = useState((rows * cols) - mines)
-  const [data, setData] = useState(createBoard(rows, cols, mines))
+  const [gameState, setGameState] = useState("notOver");
+  const rows = 8,
+    cols = 8,
+    mines = 10;
+  const [remaining, setRemaining] = useState(rows * cols - mines);
+  const [data, setData] = useState(createBoard(rows, cols, mines));
   const reset = () => {
     setData(createBoard(rows, cols, mines));
     setGameState("notOver");
-    setRemaining((rows * cols) - mines)
-  }
+    setRemaining(rows * cols - mines);
+  };
   const lowerCounter = (x) => {
     setRemaining(remaining - x);
     console.log(`decreasing counter by ${x}`);
   };
 
+  useEffect(() => {
+    if (remaining === 0) {
+      setGameState("win");
+    }
+  }, [remaining]);
+
   return (
     <>
       <h1>Minesweeper</h1>
-      <p>{gameState === "notOver" ? '🫣' : '😵'}</p>
+      <p>{gameState === "notOver" ? "🫣" : "😵"}</p>
       <p>Remaining: {remaining}</p>
-      <button onClick={reset}>Start Again</button><br />
+      <button onClick={reset}>Start Again</button>
+      <br />
       <Board
         data={data}
         setData={setData}
         lowerCounter={lowerCounter}
         setGameState={setGameState}
       />
-      {gameState === "notOver" ? '' :
+      {gameState === "notOver" ? (
+        ""
+      ) : (
         <Modal
           gameState={gameState}
           setGameState={setGameState}
           setData={setData}
           setRemaining={setRemaining}
-        />}
+        />
+      )}
+      <Win gameState={gameState} />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
